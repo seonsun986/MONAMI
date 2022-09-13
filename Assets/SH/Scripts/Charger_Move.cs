fdf46eb3bd8638b8b1076170b2c44d7577e600dc
seonsun986 <108320882+seonsun986@users.noreply.github.com>
2022-09-13 18:52:59 +0900

    .
    .

---
+++ Assets/SH/Scripts/Charger_Move.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charger_Move : MonoBehaviour
+{
+    public float walkSpeed = 10;
+    public float runSpeed = 15;
+    public float rotSpeed = 5;
+    CharacterController cc;
+    Animator anim;
+    float speed;
+    float animSpeed;
+    public enum state
+    {
+        Idle,
+        Move,
+        Attack,
+        Die
+    }
+    state State;
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        State = state.Idle;
+    }
+
+    public bool isRun;      //달리기 확인용 변수
+    float h;
+    float v;
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        Vector3 dir = h * Vector3.right + v * Vector3.forward;
+        dir.Normalize();
+
+        if (h != 0 || v != 0)
+        {
+            isRun = false;
+            // 앞이나 뒤로 움직일때만 달릴 수 있다
+            isRun = Input.GetKey(KeyCode.LeftShift);
+            speed = isRun == true ? runSpeed : walkSpeed;
+            animSpeed = isRun == true ? 1 : 0.5f;
+            
+        }
+        else
+        {
+            speed = 0;
+            animSpeed = 0;
+        }
+        anim.SetFloat("MoveSpeedAnim", animSpeed);
+        cc.Move(dir * speed * Time.deltaTime);
+
+        // 다시 회전 돌아가는걸 막기 위한 부분
+        if (!(h == 0 && v == 0))
+        {
+            // 회전하는 부분. Point 1.
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotSpeed);
+        }
+
+        // 점프
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        // 에임상태에서 움직이기
+        if(Input.GetMouseButton(1))
+        {
+            Aim();
+            anim.SetLayerWeight(1, 1);
+        }
+        if(Input.GetMouseButtonUp(1))
+        {
+            anim.SetTrigger("Move");
+            anim.SetLayerWeight(1, 0);
+        }
+
+        // 발사
+
+        if(Input.GetMouseButton(0))
+        {
+            anim.Play("Fire");
+        }
+    }
+
+    private void UpdateIdle()
+    {       
+        if (h != 0 || v != 0)
+        {
+            State = state.Move;
+            anim.SetTrigger("Move");
+        }
+    }
+
+    // 움직임이 멈추면 다시 Idle 상태로 돌아온다
+    private void UpdateMove()
+    {
+
+        // 멈추면 다시 원래 상태로
+        if (h == 0 && v == 0)
+        {
+            State = state.Idle;
+            anim.SetTrigger("Idle");
+        }
+    }
+
+    private void UpdateAttack()
+    {
+       
+    }
+
+    private void UpdateDie()
+    {
+    
+    }
+
+    void Jump()
+    {
+        anim.SetTrigger("Jump");
+    }
+
+    void Aim()
+    {
+        anim.SetTrigger("Aim");
+    }
+
+    void Attack()
+    {
+
+    }
+}
