7aa184bc1408a19280e69f1644e875e9a9e7dba0
seonsun986 <108320882+seonsun986@users.noreply.github.com>
2022-09-19 10:13:21 +0900

    .
    .

--- Assets/Scenes/SceneFlow/SceneManager_Scripts/GameManager.cs
+++ Assets/Scenes/SceneFlow/SceneManager_Scripts/GameManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using Photon.Realtime;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
@@ -14,14 +13,14 @@ public class GameManager : MonoBehaviourPunCallbacks
     }
     void Start()
     {
-        int idx = PhotonNetwork.CurrentRoom.PlayerCount - 1;
-        print("들어온 플레이어 수 : " + idx);
+        //int idx = PhotonNetwork.CountOfPlayers - 1;
+        //print("들어온 플레이어 수 : " + idx);
         //현재 들어온 플레이어 숫자에 따라 맞는 플레이어를 생성한다.
-        if (idx == 0)
+        if (PhotonNetwork.IsMasterClient)
         {
             PhotonNetwork.Instantiate("TEST_Shooter_Player_Blue", Vector3.zero, Quaternion.identity);
         }
-        else if(idx == 1)
+        else
         {
             print("블루 캐릭터 생성!");
             PhotonNetwork.Instantiate("TEST_Shooter_Player", Vector3.zero, Quaternion.identity);
